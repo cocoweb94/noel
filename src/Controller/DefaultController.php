d12fd627f0ec7a585ced9a29e62c242492359fdf
francois <fcoelho92@hotmail.com>
2020-10-07 19:35:17 +0200

    generic js

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/home", name="homepage")
      */
     public function index(Request $request): Response
     {
