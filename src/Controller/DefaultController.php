91e24d8bc03cf0ff50e267ef8c7193e274d0e616
francois <fcoelho92@hotmail.com>
2020-10-10 11:44:24 +0200

    add panier

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,10 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Cookie;
 use App\Form\Type\ContactType;
 use App\Entity\Product;
 
