f70839df642e7536987332a59a1766407ee371ca
francois <fcoelho92@hotmail.com>
2020-10-14 18:41:08 +0200

    form commande

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -21,7 +21,7 @@ class DefaultController extends AbstractController
     public function index(Request $request): Response
     {
         //----------------- GET PANIER ----------------------
-        if(array_key_exists("commande", $_COOKIE) && count(json_decode($_COOKIE["commande"])) > 0) {
+        if(array_key_exists("commande", $_COOKIE) && count(get_object_vars(json_decode($_COOKIE["commande"]))) > 0) {
             $tabCookie = get_object_vars(json_decode($_COOKIE["commande"]));
             $repository = $this->getDoctrine()->getRepository(Product::class);
             $query = $repository->createQueryBuilder('p')
@@ -79,7 +79,7 @@ class DefaultController extends AbstractController
         $countPage = count($countProducts) / getenv('LIMIT');
 
         //----------------- GET PANIER ----------------------
-        if(array_key_exists("commande", $_COOKIE) && count(json_decode($_COOKIE["commande"])) > 0) {
+        if(array_key_exists("commande", $_COOKIE) && count(get_object_vars(json_decode($_COOKIE["commande"]))) > 0) {
             $tabCookie = get_object_vars(json_decode($_COOKIE["commande"]));
             $query = $repository->createQueryBuilder('p')
                 ->where('p.stock > :stock')
@@ -109,7 +109,7 @@ class DefaultController extends AbstractController
     public function contact(Request $request): Response
     {
         //----------------- GET PANIER ----------------------
-        if(array_key_exists("commande", $_COOKIE) && count(json_decode($_COOKIE["commande"])) > 0) {
+        if(array_key_exists("commande", $_COOKIE) && count(get_object_vars(json_decode($_COOKIE["commande"]))) > 0) {
             $tabCookie = get_object_vars(json_decode($_COOKIE["commande"]));
             $repository = $this->getDoctrine()->getRepository(Product::class);
             $query = $repository->createQueryBuilder('p')
@@ -189,7 +189,7 @@ class DefaultController extends AbstractController
     {
         $repository = $this->getDoctrine()->getRepository(Product::class);
         //----------------- GET PANIER ----------------------
-        if(array_key_exists("commande", $_COOKIE) && count(json_decode($_COOKIE["commande"])) > 0) {
+        if(array_key_exists("commande", $_COOKIE) && count(get_object_vars(json_decode($_COOKIE["commande"]))) > 0) {
             $tabCookie = get_object_vars(json_decode($_COOKIE["commande"]));
             $query = $repository->createQueryBuilder('p')
                 ->where('p.stock > :stock')
@@ -200,6 +200,7 @@ class DefaultController extends AbstractController
 
             $panierProducts = $query->getResult();
         }else{
+            var_dump("toto");die;
             $this->redirectToRoute("boutique",array("panier" => "vide"),302);
         }
 
