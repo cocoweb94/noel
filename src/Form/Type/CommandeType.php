0975dc9d7951810279b300a41c9247e9d907432e
francois <fcoelho92@hotmail.com>
2020-10-15 10:46:32 +0200

    form commande

--- src/Form/Type/CommandeType.php
+++ src/Form/Type/CommandeType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -29,17 +30,15 @@ class CommandeType extends AbstractType
                 ],
                 'attr' => ['class' => 'form-control', 'placeholder' => utf8_encode ('Prénom')],
             ])
-            ->add('email', TextType::class, [
-                'constraints' => [
-                    new NotBlank(),
-                ],
-                'attr' => ['class' => 'form-control', 'placeholder' => utf8_encode ('Email')],
+            ->add('email', EmailType::class, [
+                'required'   => false,
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Email', "pattern" => "^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$"],
             ])
             ->add('paiement', ChoiceType::class, [
                 'choices'  => [
                     'Carte banquaire' => 'Carte banquaire',
                     utf8_encode ( 'Chèques' ) => 'Cheque',
-                    utf8_encode ( 'Espèces' ) => 'especes',
+                    utf8_encode ( 'Espèces' ) => 'Especes',
                 ],
                 'attr' => ['class' => 'form-control', "id" => "tarifsResa"],
             ]);
