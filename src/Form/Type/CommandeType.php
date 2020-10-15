ceebe70b52c6220c4ba6404e3472ee731486191f
francois <fcoelho92@hotmail.com>
2020-10-15 10:49:57 +0200

    form commande

--- src/Form/Type/CommandeType.php
+++ src/Form/Type/CommandeType.php
@@ -31,7 +31,7 @@ class CommandeType extends AbstractType
                 'attr' => ['class' => 'form-control', 'placeholder' => utf8_encode ('Prénom')],
             ])
             ->add('email', EmailType::class, [
-                'required'   => false,
+                'required'   => true,
                 'attr' => ['class' => 'form-control', 'placeholder' => 'Email', "pattern" => "^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$"],
             ])
             ->add('paiement', ChoiceType::class, [
